726bed73a2ad18f4f61b5a3fbec02b4bbd2aae54
Bhuvaneshwari C <165646930+bhuvaneshwari-C@users.noreply.github.com>
2024-05-23 14:27:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+
+// import Product from './Components/Product/Product';
+import './App.css';
+import RouterPage from './Components/Pages/RouterPage';
+
+
+
+function App() {
+  return (
+    <div>
+        <RouterPage/> 
+
+    </div>
+  );
+}
+
+export default App;
